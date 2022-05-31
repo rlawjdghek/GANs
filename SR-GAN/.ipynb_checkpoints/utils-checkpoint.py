8d707f404b54a52740f4babc065a484edaf413b3
rlawjdghek <rlawjdghek@skku.edu>
2022-05-31 22:24:48 +0900

    update

--- SR-GAN/.ipynb_checkpoints/utils-checkpoint.py
+++
@@ -1,8 +0,0 @@
-import os
-import torchvision
-
-def save_img(img, name):
-    if not os.path.isdir("./results"):
-        os.makedirs("./results")
-        print("make directory ./results")
-    torchvision.utils.save_image(img, "./results/{}.png".format(name))
